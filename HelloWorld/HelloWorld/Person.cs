1c3a5568fc1c57fb64c10a5bf3acfad07cf484da
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2022-11-06 18:17:25 +0100

    Reference and value types - demo2

--- HelloWorld/HelloWorld/Person.cs
+++ HelloWorld/HelloWorld/Person.cs
@@ -4,9 +4,11 @@
     {
         public string firstName;
         public string lastName;
+        public int age; 
         public void Introduce()
         {
             Console.WriteLine("My name is " + firstName + " " + lastName);
         }
+
     }
 }
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,17 +7,27 @@ namespace HelloWorld
     {
         static int Main(string[] args)
         {
-            var a = 10;
-            var b = a;
-            b++;
-            Console.WriteLine("a: {0}, b: {1}", a, b);
 
-            var array1 = new int[3] { 1, 2, 3 };
-            var array2 = array1;
-            array2[0] = 0;
-            Console.WriteLine("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]);
+            var person = new Person() { age = 25 };
+
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            MakeOld(person);
+            Console.WriteLine(person.age);
 
             return 1;
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.age += 10;
         }
     }
 }
