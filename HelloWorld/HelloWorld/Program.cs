b0eb52e896bccb2396b859cf321bccc8dd580e28
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2022-11-06 15:45:31 +0100

    2. commit - Demo Type Conversion

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var number = "1234";
+
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Prekoracenje pri konverziji tipa!");
+            }
+               
+
+            return 1;
         }
     }
 }
