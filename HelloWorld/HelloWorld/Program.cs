ed679e02b4c78d99e267cade7ede226b4f1cffb4
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2022-11-06 16:32:35 +0100

    6. commit - Removing non primitive types example

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,34 +7,6 @@ namespace HelloWorld
         static int Main(string[] args)
         {
 
-            try
-            {
-                string str = "true";
-                bool b = Convert.ToBoolean(str);
-                //console.writeline(b);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Prekoracenje pri konverziji tipa!");
-            }
-
-
-            // operators demo
-
-            var a = 10;
-            var c = 3;
-            var d = 5;
-            //Console.WriteLine(!(a != d));
-            Console.WriteLine(a > c || a < d);
-
-            // demo comments
-
-            // Single-line comment
-
-            /* 
-             *  Multi-line comment 
-             */
-
             var aleksandar = new Person();
             aleksandar.firstName = "Aleksandar";
             aleksandar.lastName = "Arsic";
