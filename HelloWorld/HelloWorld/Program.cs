ba27a3f7eb199715061f0fe9a17754891c11cbe7
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2022-11-13 13:48:43 +0100

    Example with strings

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,40 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            var sentence = "This is going to be really really really really really long text";
 
-            var numbers = new List<int>() { 1, 2, 3 };
-            numbers.Add(11);
-            numbers.Add(12);
-            numbers.Add(13);
-            numbers.AddRange(new int[4] { 20, 21, 22, 1 });
-
-            foreach (var item in numbers)
-            {
-                Console.WriteLine(item);
-            }
-
-
-            Console.WriteLine();
-            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
-            Console.WriteLine("last Index of 1: " + numbers.LastIndexOf(1));
-
-            Console.WriteLine("count: " + numbers.Count);
-
-
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                if (numbers[i] == 1)
-                    numbers.Remove(numbers[i]);
-            }
-
-
-            foreach (var item in numbers)
-            {
-                Console.WriteLine(item);
-            }
-
-            numbers.Clear();
-            Console.WriteLine("count: " + numbers.Count);
+            var summary = StringUtility.SummerizeText(sentence, 15);
+            Console.WriteLine(summary);
         }
+
     }
 }
