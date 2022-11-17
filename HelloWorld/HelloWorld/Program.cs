88833cd1c38cd35061dcca151c4f5d548001c1ab
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2022-11-17 22:17:44 +0100

    Debuging program to find error - example with error

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,16 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace HelloWorld
+namespace CSharpFundamentals
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var sentence = "This is going to be really really really really really long text";
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 3);
 
-            var summary = StringUtility.SummerizeText(sentence, 15);
-            Console.WriteLine(summary);
+            foreach (var number in smallests)
+                Console.WriteLine(number);
         }
 
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            if (list == null) 
+                throw new ArgumentOutOfRangeException("list", "Error: List is null!");
+
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between one and number of elements of the list!");
+
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(buffer);
+                smallests.Add(min);
+                buffer.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
     }
 }
