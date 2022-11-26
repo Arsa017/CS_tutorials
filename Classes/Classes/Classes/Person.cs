57bd9886b170676fd4dac8079f015ae288c5db15
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2022-11-26 14:26:53 +0100

    Working with classes - basic example

---
+++ Classes/Classes/Classes/Person.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Classes
+{
+
+    internal partial class Program
+    {
+        public class Person
+        {
+            //public string Name { get; set; }
+            //public string Username { get; set; }
+            public DateTime Birthdate { get; private set; }         // auto-implement propery
+
+
+            public Person(DateTime birthdate)
+            {
+                Birthdate = birthdate;
+            }
+
+   
+            
+            public int Age              
+            {
+                get
+                {
+                    var timespan = DateTime.Today - Birthdate;
+                    var years = timespan.Days / 365;
+
+                    return years;
+                }
+            }
+
+        }
+    
+    }
+}
