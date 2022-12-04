d59fc00e128761c04191a2191f2ee4aa4fdc43a9
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2022-12-04 11:15:01 +0100

    Lambda expression example with delegate

---
+++ AdvancedLvl/AdvancedLvl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace AdvancedLvl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // args => expression
+
+            //number => number * number
+
+            // () => ...                lambda expression with no parameters
+            // x => ...                 with one parameter
+            // (x, y, z) => ....        with multiple parameters
+
+  /*          const int factor = 5;
+
+            Func<int, int> multiplier = n => n * factor;
+
+            var result = multiplier(10);
+
+            Console.WriteLine(result);  */
+
+
+
+
+        }
+
+    }
+    
+
+}
